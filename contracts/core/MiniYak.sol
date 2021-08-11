f1d8f54404ed68e61921e5f7a200f9ed4167f46e
sidduHERE <elugamsudhiswar@gmail.com>
2021-08-12 03:58:18 +0530

    Review comment changes

--- contracts/core/MiniYak.sol
+++ contracts/core/MiniYak.sol
@@ -2,14 +2,14 @@
 pragma solidity ^0.8.0;
 
 import "../libs/openzeppelin/token/ERC20/ERC20.sol";
-import "../libs/openzeppelin/utils/math/safeMath.sol";
+import "../libs/openzeppelin/token/ERC20/utils/SafeERC20.sol";
 import "../libs/openzeppelin/utils/math/Math.sol";
 
 contract MiniYak is ERC20 {
-
+    using SafeERC20 for IERC20;
     constructor() ERC20("MiniYak", "MYAK") {}
     address public YAK = 0x59414b3089ce2AF0010e7523Dea7E2b35d776ec7;
-    
+
     /**
      * @dev Returns the number of decimals used to get its user representation.
      * For example, if `decimals` equals `2`, a balance of `505` tokens should
@@ -26,7 +26,7 @@ contract MiniYak is ERC20 {
      */
     function moon(uint amount, address to) external {
         uint mint_amount = Math.min(amount, IERC20(YAK).balanceOf(msg.sender));
-        require(IERC20(YAK).transferFrom(msg.sender,address(this),mint_amount));
+        IERC20(YAK).safeTransferFrom(msg.sender,address(this),mint_amount);
         _mint(to, mint_amount);
     }
 
@@ -39,7 +39,7 @@ contract MiniYak is ERC20 {
     function unmoon(uint amount, address to) external {
         uint burn_amount = Math.min(amount, this.balanceOf(msg.sender));
         _burn(msg.sender, burn_amount);
-        require(IERC20(YAK).transfer(to, burn_amount));
+        IERC20(YAK).safeTransfer(to, burn_amount);
     }
 
 
